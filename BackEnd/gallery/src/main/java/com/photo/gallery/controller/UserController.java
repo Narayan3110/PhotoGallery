8b75bdb520195e1323f88d8f3bb775636e8d9ae2
Rushi <rushikesh.aher027@gmail.com>
2025-01-30 13:53:11 +0530

    Added AboutUs Page and Resolve Conflict

--- BackEnd/gallery/src/main/java/com/photo/gallery/controller/UserController.java
+++ BackEnd/gallery/src/main/java/com/photo/gallery/controller/UserController.java
@@ -1,14 +1,6 @@
 package com.photo.gallery.controller;
-<<<<<<< HEAD
-
-
 import com.photo.gallery.model.User;
 import com.photo.gallery.service.UserService;
-
-=======
-import com.photo.gallery.model.User;
-import com.photo.gallery.service.UserService;
->>>>>>> 59cc755ddb6dfc7d994b60c90e075b5117cfc289
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,18 +23,6 @@ public class UserController {
 	
 	@PostMapping("/register")
 	public ResponseEntity<User> registerUser(@RequestBody User user) {
-<<<<<<< HEAD
-		User savedUser = userService.saveUser(user);
-		return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
-	}
-
-	@PostMapping("/login")
-	public ResponseEntity<Map<String,String>> login(@RequestBody User user) {
-		return userService.loginUser( user.getUserName(),user.getPassword());
-	        
-	}
-
-=======
 	    // Ensure the bidirectional relationship is set
 		System.out.println(user);
 	    if (user.getUserProfile() != null) {
@@ -62,5 +42,4 @@ public class UserController {
 	        
 	}
 
->>>>>>> 59cc755ddb6dfc7d994b60c90e075b5117cfc289
 }
